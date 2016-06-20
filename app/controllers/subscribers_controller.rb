31fe715bdfc1124046c1d97ee447f862349c74ad
Atish <amaske@gmail.com>
2016-06-20 12:11:17 +0530

    code refrractor

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,25 +1,25 @@
 class SubscribersController < ApplicationController
-  before_action :set_subscriber, only: [:show, :edit, :update, :destroy]
+  #before_action :set_subscriber, only: [:show, :edit, :update, :destroy]
 
-  # GET /subscribers
-  # GET /subscribers.json
-  def index
-    @subscribers = Subscriber.all
-  end
+  # # GET /subscribers
+  # # GET /subscribers.json
+  # def index
+  #   @subscribers = Subscriber.all
+  # end
 
-  # GET /subscribers/1
-  # GET /subscribers/1.json
-  def show
-  end
+  # # GET /subscribers/1
+  # # GET /subscribers/1.json
+  # def show
+  # end
 
-  # GET /subscribers/new
+  # # GET /subscribers/new
   def new
     @subscriber = Subscriber.new
   end
 
-  # GET /subscribers/1/edit
-  def edit
-  end
+  # # GET /subscribers/1/edit
+  # def edit
+  # end
 
   # POST /subscribers
   # POST /subscribers.json
@@ -40,33 +40,33 @@ class SubscribersController < ApplicationController
 
   # PATCH/PUT /subscribers/1
   # PATCH/PUT /subscribers/1.json
-  def update
-    respond_to do |format|
-      if @subscriber.update(subscriber_params)
-        format.html { redirect_to @subscriber, notice: 'Subscriber was successfully updated.' }
-        format.json { render :show, status: :ok, location: @subscriber }
-      else
-        format.html { render :edit }
-        format.json { render json: @subscriber.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @subscriber.update(subscriber_params)
+  #       format.html { redirect_to @subscriber, notice: 'Subscriber was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @subscriber }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @subscriber.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /subscribers/1
   # DELETE /subscribers/1.json
-  def destroy
-    @subscriber.destroy
-    respond_to do |format|
-      format.html { redirect_to subscribers_url, notice: 'Subscriber was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @subscriber.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to subscribers_url, notice: 'Subscriber was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_subscriber
-      @subscriber = Subscriber.find(params[:id])
-    end
+    # def set_subscriber
+    #   @subscriber = Subscriber.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def subscriber_params
